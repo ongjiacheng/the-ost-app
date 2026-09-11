8357f77b015ca9f23a66f50768f647ce470eb639
Ong Jia Cheng <ongjiacheng.2002@gmail.com>
2026-09-11 16:21:33 +0800

    Adjust icon spacing

--- app/root.tsx
+++ app/root.tsx
@@ -85,19 +85,19 @@ function Header() {
                             <Box component="img" src={logo} alt="OST Logo" sx={{ width: 120, height: 32 }} />
                         </IconButton>
                         <Box>
-                            <IconButton component={Link} to="/walk" aria-label="Walk">
+                            <IconButton component={Link} to="/walk" aria-label="Walk" sx={{ p: 0.5 }}>
                                 <Box component="img" src={walk} alt="Walk" sx={{ width: 36, height: 36 }} />
                             </IconButton>
-                            <IconButton component={Link} to="/cycle" aria-label="Cycle">
+                            <IconButton component={Link} to="/cycle" aria-label="Cycle" sx={{ p: 0.5 }}>
                                 <Box component="img" src={cycle} alt="Cycle" sx={{ width: 36, height: 36 }} />
                             </IconButton>
-                            <IconButton component={Link} to="/train/mrt" aria-label="MRT">
+                            <IconButton component={Link} to="/train/mrt" aria-label="MRT" sx={{ p: 0.5 }}>
                                 <Box component="img" src={mrt} alt="MRT" sx={{ width: 36, height: 36 }} />
                             </IconButton>
-                            <IconButton component={Link} to="/bus" aria-label="Bus">
+                            <IconButton component={Link} to="/bus" aria-label="Bus" sx={{ p: 0.5 }}>
                                 <Box component="img" src={bus} alt="Bus" sx={{ width: 36, height: 36 }} />
                             </IconButton>
-                            <IconButton component={Link} to="/train/lrt" aria-label="LRT">
+                            <IconButton component={Link} to="/train/lrt" aria-label="LRT" sx={{ p: 0.5 }}>
                                 <Box component="img" src={lrt} alt="LRT" sx={{ width: 36, height: 36 }} />
                             </IconButton>
                         </Box>
